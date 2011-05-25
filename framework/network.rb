edd9980887b2613189a4eab80ec1943e75d3a1cb
Andrew Burns <andrew@wyominghealthfairs.com>
2011-05-25 13:22:35 -0600

    Added methods to the network framework that return the IPs the machine has as well as the on-network method

--- framework/network.rb
+++ framework/network.rb
@@ -1,4 +1,12 @@
 class Network
+  def self.ips
+    ips = []
+    ifconfig = %x[ifconfig]
+    m = ifconfig.scan /^\s*inet\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/i
+    m.each { |ip| ips << ip[0] } if m
+    ips
+  end
+
   def self.default_gateway
     routes = %x[netstat -nr]
     rx = /^default\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/i
@@ -19,5 +27,23 @@ class Network
     ping = self.ping host
     return false if ping.nil?
     return true unless ( ping < 0 || ping > timeout )
+  end
+
+  def self.on_network network
+    # Simple test to identify if you are on the specified network
+    # at the end it will turn the network specified into a ^REGEX
+    #
+    # Can use * wildcard which will translate to \d{1,3} in regex parlance
+
+    rx_str = "^#{network}"
+    rx_str.gsub! '*', '\d{1,3}'
+    rx_str.gsub! '.', '\.'
+    rx = Regexp.new rx_str
+    ips.each do |ip|
+      return true if rx.match ip
+    end
+
+    # nope :(
+    false
   end
 end
